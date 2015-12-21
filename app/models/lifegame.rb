281c16fdc9185d93db8f5e963aeff2fe96f3422d
murakami <tmaqua007@gmail.com>
2015-12-22 00:56:49 +0900

    不必要なコントローラの削除

--- app/models/lifegame.rb
+++
@@ -1,2 +0,0 @@
-class Lifegame < ActiveRecord::Base
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :cell_automatons
-  resources :lifegames
   get 'cell_automatons/show_cell/:id' => 'cell_automatons#show_cell', as: :show_cell
   get 'play/index'
 
