7225d0dcf26b873f1a03719458a169e25cfd01c6
murakami <tmaqua007@gmail.com>
2016-10-07 14:50:26 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,10 @@
-server '131.206.19.97', user: 'murakami', roles: %w{app db web}
+# server '131.206.19.97', user: 'murakami', roles: %w{app db web}
 set :ssh_options, {
     keys: %w(~/.ssh/ishizaka-lab/id_rsa),
     forward_agent: true,
     auth_methods: %w(publickey)
   }
 
+user: 'murakami', roles: %w{app db web}
 set :branch, "master"
 role :web, "ishizaka-lab"
